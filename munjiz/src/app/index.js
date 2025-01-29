11825dbd550c423176fcc15a7945083899b77ff0
Hoby21 <hobylobytoby@gmail.com>
2025-01-29 17:58:50 +0300

    test

--- munjiz/src/app/index.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import "./index.css";
-import App from "./App";
-import "bootstrap/dist/css/bootstrap.css";
-import "font-awesome/css/font-awesome.css";
-
-createRoot.render(<App />, document.getElementById("root"));
