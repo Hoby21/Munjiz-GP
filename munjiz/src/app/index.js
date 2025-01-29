fb386e5ff97257a879f8c8401d2111992ddc79d9
Hoby21 <hobylobytoby@gmail.com>
2025-01-29 17:35:51 +0300

    test

---
+++ munjiz/src/app/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import "./index.css";
+import App from "./App";
+import "bootstrap/dist/css/bootstrap.css";
+import "font-awesome/css/font-awesome.css";
+
+createRoot.render(<App />, document.getElementById("root"));
